25841f1f48142f2edbae5ada94063c2ae70c572d
Renato Souza <renatoosoouzaa26@gmail.com>
2022-03-10 15:51:25 -0300

    testando postman

--- exercicio.js
+++
@@ -1,8 +0,0 @@
-const dbs = require("./db")
-const user = {
-    name: "Joana",
-    email: "j.ana@gmail.com",
-    password: "abc.123#"
-}
-const createdItem = dbs.create(user)
-console.log(createdItem)
--- main.js
+++ main.js
@@ -6,6 +6,24 @@ const db = require("./db")
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.get("/usuario", (request, response) => {
+    return response.json({ users: db.findAll() })
+})
+
 app.listen(3030, () => {
     console.log("Esse servidor está rodando em http://localhost:3030")
 })
+
+
+
+
+// Exercicio de CRUD
+// Utilizando as 5 funções encontradas em db, crie 5 endpoints para o recurso "usuario".
+// (Leia em README para saber mais sobre as funções) 
+/*        O recurso usuario deve ter as seguintes propriedades com seus respectivos 
+tipos: {
+  name: String,         
+  email: String,          
+  password: String   
+   } 
+*/
