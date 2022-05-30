80c0ea355779d21374b59663ed972686fc375405
ludmila <ludmilakdk@gmail.com>
2022-05-30 09:34:55 -0300

    testando o codigo

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 const express = require("express")
 const app = express()
-const db = require("./db")
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get("/", (request, response) => {
-    response.json({ message: db.findAll() })
+    response.json({ message: "ola mundo" })
 })
 
 app.get("/usuario", (request, response) => {
@@ -18,31 +17,11 @@ app.get("/pokemon", (request, response) => {
 })
 
 app.post("/pokemon", (request, response) => {
-    const {name, email, password} = request.body
-    
-    return response.json(db.create({name, email, password}))
-})
-
-app.put("/pokemon/:id", (request, response) => {
-    const id = request.params
-    const {name, email, password} = request.body
-    
-    return response.json(db.updateById(id, {name, email, password}))
+    const body = request.body
+    console.log(body)
+    response.json({ message: "foi adicionado um novo pokemon" })
 })
 
 app.listen(3030, () => {
-    console.log("Esse servidor está rodando em http://localhost:3030")
+    console.log("Esse servidor está rodando em http://localhost:3030/%22")
 })
-/*
-
-// Exercicio de CRUD
-// Utilizando as 5 funções encontradas em db, crie 5 endpoints para o recurso "usuario".
-// (Leia em README para saber mais sobre as funções)
-/* 
-    O recurso usuario deve ter as seguintes propriedades com seus respectivos tipos:
-    { 
-        name: String, 
-        email: String, 
-        password: String 
-    }
-*/
