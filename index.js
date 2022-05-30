9904e75eb465289e7ea60ec2e6d0b3f156d38a05
ludmila <ludmilakdk@gmail.com>
2022-05-30 09:23:19 -0300

    pequenas alteracoes para usuario

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
-//const db = require("./db")
-import {db} from './db' 
+const express = require("express")
+const app = express()
+const db = require("./db")
 
-const items = db.findAll()
-console.log(items)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get("/", (request, response) => {
+    response.json({ message: db.findAll() })
+})
+
+app.get("/usuario", (request, response) => {
+    response.json({ users: ["joao", "maria", "jose"] })
+})
+
+app.get("/usuario", (request, response) => {
+    response.json({ message: "charmander" })
+})
+
+app.post("/usuario", (request, response) => {
+    const {name, email, password} = request.body
+    
+    return response.json(db.create({name, email, password}))
+})
+
+app.put("/usuario/:id", (request, response) => {
+    const id = request.params
+    const {name, email, password} = request.body
+    
+    return response.json(db.updateById(id, {name, email, password}))
+})
+
+app.listen(3030, () => {
+    console.log("Esse servidor está rodando em http://localhost:3030")
+})
+/*
+
+// Exercicio de CRUD
+// Utilizando as 5 funções encontradas em db, crie 5 endpoints para o recurso "usuario".
+// (Leia em README para saber mais sobre as funções)
+/* 
+    O recurso usuario deve ter as seguintes propriedades com seus respectivos tipos:
+    { 
+        name: String, 
+        email: String, 
+        password: String 
+    }
+*/
