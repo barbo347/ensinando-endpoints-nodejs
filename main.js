b10d61fadbf384bf3366abcbb6c1ac1694a12ee6
nicofercavv-dev <nicoleferreira281@gmail.com>
2022-09-29 16:18:02 -0300

    Endpoints criados

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const express = require("express")
-const app = express()
-const db = require("./db")
+const express = require("express");
+const app = express();
+const db = require("./db");
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Exercicio de CRUD
 // Utilizando as 5 funções encontradas em db, crie 5 endpoints para o recurso "usuario".
@@ -17,6 +17,34 @@ app.use(express.urlencoded({ extended: true }))
     }
 */
 
+app.get("/users", (req, res) => res.send(db.findAll()));
+
+app.get("/user", (req, res) => {
+  const { id } = req.body;
+
+  return res.send(db.findById(id));
+});
+
+app.post("/user", (req, res) => {
+  const newUser = req.body;
+  db.create(newUser);
+
+  return res.send(db.findAll());
+});
+
+app.put("/user", (req, res) => {
+  const { id, user } = req.body;
+  db.updateById(id, user);
+
+  return res.send(db.findAll());
+});
+
+app.delete("/users", (req, res) => {
+  const { id } = req.body;
+
+  return res.send(db.remove(id));
+});
+
 app.listen(3030, () => {
-  console.log("Esse servidor está rodando em http://localhost:3030")
-})
+  console.log("Esse servidor está rodando em http://localhost:3030");
+});
